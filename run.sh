887951437771a3d85dd8c9865c04f5173bd5d72c
wunhuang <wunhuang@amd.com>
2023-01-31 12:22:02 +0100

    update running script

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ MODEL=${2:-"bert"}
 ddp_method=${3:-"torchdpp"}
 deepspeed_conf=$4
 
-MASTER_NODE=`echo $SLURM_NODELIST | sed -e 's/\[\([0-9]\).*\]/\1/g' | sed -e 's/,.*//g'`
+#MASTER_NODE=`echo $SLURM_NODELIST | sed -e 's/\[\([0-9]\).*\]/\1/g' | sed -e 's/,.*//g'`
 
-MASTER_NODE=${MASTER_NODE:-`hostname`}
+#MASTER_NODE=${MASTER_NODE:-`hostname`}
 SLURM_NODEID=${SLURM_NODEID:-0}
 SLURM_NTASKS=${SLURM_NTASKS:-1}
 
@@ -17,17 +17,26 @@ HF_PATH=${HF_PATH:-`pwd`}
 HF_HOME=${HF_HOME:-"${HF_PATH}/nas_share"}
 batch_size=${batch_size:-24}
 
-CMD="python3 -m torch.distributed.launch --node_rank ${SLURM_NODEID} --nnodes ${SLURM_NTASKS} --master_addr ${MASTER_NODE} --master_port 23456 --nproc_per_node ${USEGPUS}"
+source_prefix_enable=false
 
+CMD=""
 
 if echo "bert" | grep -i "^${MODEL}$" > /dev/null ; then
     CMD+=" ${HF_PATH}/examples/pytorch/language-modeling/run_mlm.py"
     CMD+=" --model_name_or_path bert-large-uncased --dataset_name wikitext --dataset_config_name wikitext-2-raw-v1 --output_dir /tmp/test-mlm-bbu"
 elif echo "bart" | grep -i "^${MODEL}$" > /dev/null ; then
     CMD+=" $HF_PATH/examples/pytorch/translation/run_translation.py"
-    CMD+=" --model_name_or_path facebook/bart-large --dataset_name wmt16 --dataset_config ro-en --output_dir /tmp/test-mlm-bbu --output_dir /tmp/test-mlm-bbu"
+    CMD+=" --model_name_or_path facebook/bart-large --dataset_name wmt16 --dataset_config ro-en --output_dir /tmp/test-mlm-bbu"
     CMD+=" --label_smoothing 0.1"
     CMD+=" --predict_with_generate --source_lang en --target_lang ro --warmup_steps 5"
+    batch_size=60
+elif echo "t5-large" | grep -i "^${MODEL}$" > /dev/null ; then
+    CMD+=" $HF_PATH/examples/pytorch/translation/run_translation.py"
+    CMD+=" --model_name_or_path t5-large --dataset_name wmt16 --dataset_config ro-en --output_dir /tmp/tst-translation"
+    CMD+=" --label_smoothing 0.1"
+    CMD+=" --predict_with_generate --source_lang en --target_lang ro --warmup_steps 5"
+    source_prefix_enable=true
+    batch_size=30
 else
     "this model not support"
     exit
@@ -39,6 +48,10 @@ if [ "$ddp_method" == "deepspeed" ]; then
     CMD+=" --deepspeed=${deepspeed_conf}"
 fi
 
-
-echo $CMD
-$CMD
+if $source_prefix_enable ; then
+    echo "$CMD --source_prefix \"translate English to Romanian: \""
+    python3 -m torch.distributed.launch --node_rank ${SLURM_NODEID} --nnodes ${SLURM_NTASKS} --master_addr ${MASTER_NODE} --master_port 23456 --nproc_per_node ${USEGPUS}${CMD} --source_prefix "translate English to Romanian: "
+else
+    echo $CMD
+    python3 -m torch.distributed.launch --node_rank ${SLURM_NODEID} --nnodes ${SLURM_NTASKS} --master_addr ${MASTER_NODE} --master_port 23456 --nproc_per_node ${USEGPUS}${CMD}
+fi
