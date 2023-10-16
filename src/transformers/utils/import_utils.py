bdafd6e8c221c1a5a2facc8f45e6025b80cb4a8a
Jason Furmanek <Jason.Furmanek@amd.com>
2023-10-16 17:50:36 +0000

    Fix Tensorflow package check

--- src/transformers/utils/import_utils.py
+++ src/transformers/utils/import_utils.py
@@ -42,16 +42,20 @@ def _is_package_available(pkg_name: str, return_version: bool = False) -> Union[
     # Check we're not importing a "pkg_name" directory somewhere but the actual library by trying to grab the version
     package_exists = importlib.util.find_spec(pkg_name) is not None
     package_version = "N/A"
-    if package_exists:
-        try:
-            package_version = importlib.metadata.version(pkg_name)
-            package_exists = True
-        except importlib.metadata.PackageNotFoundError:
-            package_exists = False
-        logger.debug(f"Detected {pkg_name} version {package_version}")
     if return_version:
+        if package_exists:
+            try:
+                package_version = importlib.metadata.version(pkg_name)
+                package_exists = True
+            except importlib.metadata.PackageNotFoundError:
+                package_exists = False
+        logger.debug(f"Detected {pkg_name} version {package_version}")
         return package_exists, package_version
     else:
+        if package_exists:
+            package_exists = True
+        else:
+            package_exists = False
         return package_exists
 
 
