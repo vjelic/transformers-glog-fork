a240416abab2378b66eb0ff648a61daebd102be8
Cemberk <Cem.bozkus@amd.com>
2024-11-08 17:56:46 -0800

    Create fork-maintenance.yml (#61)
    Add fork maintenance system and add skip for multigpu issue

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -3067,6 +3067,7 @@ class ModelTesterMixin:
             self.assertTrue(number_of_different_tokens <= max_differences)  # accept up to 10% mismatch
 
     @require_non_xpu
+    @skipIfRocm
     @require_torch_multi_gpu
     def test_multi_gpu_data_parallel_forward(self):
         config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
