f5ca7d7f460b99b2280cbc106f0ead7cdb611c25
Cemberk <Cem.bozkus@amd.com>
2024-12-03 09:41:49 -0600

    fix conflict in trainer.py (#75)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2515,6 +2515,9 @@ class Trainer:
                     # Since we perform prefetching, we need to manually set sync_gradients
                     self.accelerator.gradient_state._set_sync_gradients(do_sync_step)
 
+                    if (self.state.global_step == 10):
+                        start_train_stable_time = time.time()
+
                     if self.args.include_num_input_tokens_seen:
                         main_input_name = getattr(self.model, "main_input_name", "input_ids")
                         if main_input_name not in inputs:
